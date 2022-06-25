faeab98f6667923ba77f6879a1ef968bb7b25e67
whdms2008 <whdms1107@naver.com>
2022-06-25 19:57:26 +0900

    시험 성적

---
+++ boj/boj9498.py
@@ -0,0 +1,13 @@
+import sys
+
+a = int(sys.stdin.readline())
+if 90 <= a <= 100:
+    print("A")
+elif 80 <= a <= 89:
+    print("B")
+elif 70 <= a <= 79:
+    print("C")
+elif 60 <= a <= 69:
+    print("D")
+else:
+    print("F")
