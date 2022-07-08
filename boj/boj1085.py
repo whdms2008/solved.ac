a2f5a603cbe42503d2ef29b6cac6a25ed9e18d7f
whdms2008 <whdms1107@naver.com>
2022-07-08 17:31:58 +0900

    직사각형에서 탈출

--- boj/boj1085.py
+++ boj/boj1085.py
@@ -1,13 +1,4 @@
 x, y, w, h = list(map(int, input().split()))
-
-if w - x > h - y:
-    if y > h - y:
-        print(h - y)
-    else:
-        print(y, h-y)
-        print(y)
-else:
-    if x > w - x:
-        print(w-x)
-    else:
-        print(x)
+w = w-x
+h = h-y
+print(min(x,y,w,h))
