46dde98d1ea9e973d1ac3ad890858ed858c0ef84
whdms2008 <whdms1107@naver.com>
2022-07-09 17:40:29 +0900

    ACM 호텔

--- boj/boj10250.py
+++ boj/boj10250.py
@@ -1,7 +1,7 @@
 for _ in range(int(input())):
     a = list(map(int, input().split()))
-    cnt = 0
+    cnt = 1
     while a[2]-a[0]>0:
         a[2] -= a[0]
         cnt += 1
-    print(n*100 + r_num)
+    print(a[2]*100 + cnt)
