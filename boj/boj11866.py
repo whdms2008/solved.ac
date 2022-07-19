1c604787b6105604db369e9d093bed2a6231ad4c
whdms2008 <whdms1107@naver.com>
2022-07-19 21:56:10 +0900

    요세푸스 문제 (덜함)

---
+++ boj/boj11866.py
@@ -0,0 +1,14 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().strip().split(" "))
+a = [i for i in range(1, n+1)]
+result = []
+for i in range(n-1):
+    for j in range(n):
+        if a[n]
+        a[a.index(k)] = -1
+        result.append(k)
+        k += 3
+        if k > n:
+            k -= n
+print(f"<{str(result)[1:-1]}>")
