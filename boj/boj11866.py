6329887cf0250761e48aae8d06c19553ac13b01a
whdms2008 <whdms1107@naver.com>
2022-07-22 16:23:37 +0900

    요세푸스 문제 0

--- boj/boj11866.py
+++ boj/boj11866.py
@@ -1,17 +1,16 @@
 import sys
 
 n, k = map(int, sys.stdin.readline().strip().split(" "))
-a = [i for i in range(1, n+1)]
+a = [i for i in range(1, n + 1)]
 result = []
-cnt = a[a.index(k)]
+cnt = 0
 for i in range(n):
-    check = len(a[cnt:]) + k
-    if cnt > len(a):
-        cnt = len(a)-check
-    if len(a) < k:
-        cnt = len(a)
-    print(a, cnt-1, check)
-    result.append(a[cnt-1])
-    a.pop(cnt-1)
-    cnt += k-1
+    for j in range(k-1):
+        if cnt > len(a)-1:
+            cnt = 0
+        cnt += 1
+    if cnt > len(a)-1:
+        cnt = 0
+    result.append(a[cnt])
+    del a[cnt]
 print(f"<{str(result)[1:-1]}>")
