da8352c27f04722f3eb703277e7084874db0a6e0
whdms2008 <whdms1107@naver.com>
2022-08-05 20:08:38 +0900

    나무 자르기... 다시해보자

--- boj/boj2805.py
+++ boj/boj2805.py
@@ -1,17 +1,17 @@
 from sys import stdin
 
-N, M = list(map(int,stdin.readline().split(" ")))
-height = list(map(int,stdin.readline().split(" ")))
-a = list(range(N))
-start = min(height)
-end = max(height)
+N, M = map(int, stdin.readline().split(" "))
+height = list(map(int, stdin.readline().split(" ")))
+start, end = 1, max(height)
 while start <= end:
-    mid_cut = (start + end) // 2
-    a = sum([height[i]-mid_cut if height[i]-mid_cut > 0 else 0 for i in range(N)])
-    if a == M:
-        print(mid_cut)
-        break
-    elif a > M:
-        start = mid_cut + 1
+    mid = (start + end) // 2
+    cut = 0
+    for i in height:
+        if i >= mid:
+            cut += i - mid
+
+    if cut >= M:
+        start = mid + 1
     else:
-        end = mid_cut -1
+        end = mid - 1
+print(end)
