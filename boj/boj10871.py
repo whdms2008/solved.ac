716287818e31555d3ee73aa8a53b5cbc0c1b4040
whdms2008 <45191177+whdms2008@users.noreply.github.com>
2023-09-25 14:06:19 +0900

    BOJ 10871 수정

--- boj/boj10871.py
+++ boj/boj10871.py
@@ -1,11 +1,2 @@
-import sys
-
-cnt, max = map(int, sys.stdin.readline().split())
-for i in range(cnt):
-    try:
-        x = list(map(int, sys.stdin.readline().split()))
-        for i in x:
-            if i < max:
-                print(i)
-    except:
-        break
+X = int(input().split()[1])
+print(*[n for n in map(int, input().split()) if X > n])
