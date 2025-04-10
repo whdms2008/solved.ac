1650928ce493d08ba5ecab9a97cf283cb99e718d
joeun <whdms1107@gmail.com>
2025-04-10 11:29:43 +0900

    BOJ 1026 SOLVED

---
+++ boj/boj1026.py
@@ -0,0 +1,43 @@
+# 1026번 : 보물
+# https://www.acmicpc.net/problem/1026
+
+# 문제 정의
+"""
+옛날 옛적에 수학이 항상 큰 골칫거리였던 나라가 있었다.
+이 나라의 국왕 김지민은 다음과 같은 문제를 내고 큰 상금을 걸었다.
+
+길이가 N인 정수 배여 A와 B가 있다.
+다음과 같은 함수 S를 정의하자.
+
+S = A[0] x B[0] + ... + A[N-1] x B[N-1]
+
+S의 값을 가장 작게 만들기 위해 A의 수를 재배열하자.
+단, B에 있는 수는 재배열하면 안된다.
+
+S의 최솟값을 출력하는 프로그램을 작성하시오.
+"""
+
+# 입력
+"""
+첫째 줄에 N이 주어진다.
+둘째 줄에는 A에 있는 N개의 수가 순서대로 주어지고,
+셋째 줄에는 B에 있는 수가 순서대로 주어진다.
+N은 50보다 작거나 같은 자연수이다.
+
+A와 B의 각 원소는 100보다 작거나 같은 음이 아닌 정수이다.
+"""
+import sys
+
+input = sys.stdin.readline
+
+
+N = int(input())
+
+A = sorted(map(int, input().split(" ")))
+B = sorted(map(int, input().split(" ")), reverse=True)
+S = 0
+
+for i in range(N):
+    S += A[i] * B[i]
+
+print(S)
