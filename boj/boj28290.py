b028e60cb0b8c177f941bcb3c6d6473aaf863b55
JoEun <whdms1107@gmail.com>
2025-11-22 16:30:57 +0900

    28290
    Signed-off-by: JoEun <whdms1107@gmail.com>

---
+++ boj/boj28290.py
@@ -0,0 +1,12 @@
+S = input()
+
+if "fdsajkl;" == S or "jkl;fdsa" == S:
+    print("in-out")
+elif "asdf;lkj" == S or ";lkjasdf" == S:
+    print("out-in")
+elif "asdfjkl;" == S:
+    print("stairs")
+elif ";lkjfdsa" == S:
+    print("reverse")
+else:
+    print("molu")
