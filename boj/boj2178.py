f310f69f4c7fe25b8d42da12a2169684240cd007
JoEun <whdms1107@gmail.com>
2025-11-22 20:31:42 +0900

    2178 미로
    Signed-off-by: JoEun <whdms1107@gmail.com>

--- boj/boj2178.py
+++ boj/boj2178.py
@@ -1,30 +1,53 @@
-from sys import stdin
+"""
+| 설명 |
+
+NxM크기의 배열로 표현되는 미로가 있다.
+
+미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다.
+이러한 미로가 주어졌을 때, (1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 지나야 하는
+최소의 칸 수를 구하는 프로그램을 작성하시오.
+한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동ㅎ알 수 있다.
+"""
+
+"""
+[ 입력 ]
+첫째 줄에 두 정수 N, M(2<=N, M <= 100)이 주어진다.
+다음 N개의 줄에는 M개의 정수로 미로가 주어진다.
+각각의 수들은 붙어서 입력으로 주어진다.
+"""
+
+"""
+[ 출력 ]
+첫째 줄에 지나야 하는 최소의 칸 수를 출력한다.
+항상 도착위치로 이동할 수 있는 경우만 입력으로 주어진다.
+"""
+
 from collections import deque
 
-N, M = map(int, stdin.readline().split())
+def bfs(graph, N, M):
+    q = deque([(0, 0)])
+    graph[0][0] = 1
+    while q:
+        x, y = q.popleft()
+        is_check = False
+        for dx2, dy2 in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+            dx = x + dx2
+            dy = y + dy2
+            if not (0 <= dx < M) or  not (0 <= dy < N) :
+                continue
+            if graph[dy][dx] != 0 and graph[dy][dx] == 1:
+                q.append((dx, dy))
+                graph[dy][dx] = graph[y][x] + 1
+                is_check = True
+            if dy+1 == N and dx+1 == M:
+                break
+        if y+1 == N and x+1 == M:
+            return graph[y][x]
+        if not is_check:
+            graph[y][x] = 0
 
-queue = deque()
+N, M = map(int, input().split(" "))
 
-maze = [stdin.readline().strip() for _ in range(N)]
+maze = [list(map(int, ",".join(input()).split(","))) for _ in range(N)]
 
-n = 2
-m = 2
-cnt = 0
-while True:
-    cnt += 1
-    if 0 < n - 1 < N:
-        queue.append(maze[n - 1][m])
-    if 0 < m - 1 < M:
-        queue.append(maze[n][m - 1])
-    if n + 1 < N:
-        queue.append(maze[n + 1][m])
-    if m + 1 < M:
-        queue.append(maze[n][m + 1])
-    print(queue)
-    n += 1
-    m += 1
-    for i in queue:
-        print(i)
-    print(queue)
-    if cnt == 1:
-        break
+print(bfs(maze, N, M))
